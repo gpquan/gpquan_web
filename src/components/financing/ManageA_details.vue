7f1598f7055b909225e05f1ae51bcebe52f95a22
844312006@qq.com <844312006@qq.com>
2019-06-14 17:16:47 +0800

    静态页面 提交

--- src/components/financing/ManageA_details.vue
+++ src/components/financing/ManageA_details.vue
@@ -1,19 +1,15 @@
 <template>
-	<div class="container">  <!--外层盒子-->
+	<div class="container">
+		<!--外层盒子-->
 		<div class="head_card">
 			<div>
 				<span class="head_card_title">项目名片</span>
-				<nut-button 
-				  type="light" 
-				  shape="circle" 
-				  small
-				  id="btn"
-				>
-				  皓月灰
+				<nut-button type="light" shape="circle" small id="btn">
+					皓月灰
 				</nut-button>
 			</div>
 			<div class="head_card_icon">
-				
+
 			</div>
 
 		</div>
@@ -33,7 +29,7 @@
 						</div>
 					</div>
 				</div>
-			
+
 				<div class="step-item-box">
 					<div class="step-item-left step-circle-doing">
 						<div class="step-circle"><span class="step-circle-inner"></span></div>
@@ -90,11 +86,11 @@
 						</div>
 					</div>
 				</div>
-			
-				
+
+
 			</div>
 		</div>
-		
+
 		<div class="bot-alert-box">
 			<div class="pop-mask"></div>
 			<div class="pop-body">
@@ -103,12 +99,7 @@
 					<span class="alert-head">编辑</span>
 				</div>
 				<div class="alert-body">
-					 <el-input
-					    type="textarea"
-						:rows="2"
-						placeholder="请输入内容"
-						:autosize="{ minRows: 7, maxRows:8}"
-						v-model="textarea">
+					<el-input type="textarea" :rows="2" placeholder="请输入内容" :autosize="{ minRows: 7, maxRows:8}" v-model="textarea">
 					</el-input>
 				</div>
 				<div class="alert-foot">
@@ -125,51 +116,85 @@
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-        textarea:'',
-  };
-  },
-  methods: {
-    showPop:function(type){
-    	if(type == 1){
-    		$(".bot-alert-box").fadeIn(10)
-    		setTimeout(function(){
-    			$(".pop-body").slideDown(200)
-    		},10)
-    	}else{
-    		$(".pop-body").slideUp(200)
-    		setTimeout(function(){
-    			$(".bot-alert-box").fadeOut(10)
-    		},200)
-    	}
-    }
-  },
-  }
+	export default {
+		data() {
+			return {
+				textarea: '',
+			};
+		},
+		mounted() {
+			let userId = JSON.parse(sessionStorage.getItem("userInfo")).id;
+			// console.log()
+			this.$post("/api/getProjectInvestProgress", {
+				userId: 3,
+				page: "1"
+			}).then(
+				res => {
+					for (let i = 0; i < res.data.length; i++) {
+						res.data[i].status = 2;
+						if (res.data[i].projects.length < 3) {
+							res.data[i].maxLength = res.data[i].projects.length;
+						} else {
+							res.data[i].maxLength = 3;
+						}
+					}
+					for (let i = 0; i < res.data.length; i++) {
+						//    this.statusList.show
+						this.statusList.push(0)
+						console.log(res.data)
+						// this.statusList[i].length=this.ListData.length
+						//    this.ListData[i].ISshow=false
+						// console.log(this.ListData[i])
+					}
+					console.log(res.data);
+					this.MaxList = res.data;
+					console.log(res);
+				}
+			);
+		},
+		methods: {
+			showPop: function(type) {
+				if (type == 1) {
+					$(".bot-alert-box").fadeIn(10)
+					setTimeout(function() {
+						$(".pop-body").slideDown(200)
+					}, 10)
+				} else {
+					$(".pop-body").slideUp(200)
+					setTimeout(function() {
+						$(".bot-alert-box").fadeOut(10)
+					}, 200)
+				}
+			}
+		},
+	}
 </script>
 
 <style lang="scss" scoped>
-	/deep/.el-textarea{
+	/deep/.el-textarea {
 		min-height: 25vh
 	}
-	/deep/.el-textarea__inner{
+
+	/deep/.el-textarea__inner {
 		min-height: 25vh;
 		width: 90%;
 		margin: 0 auto;
 		font-size: 16px;
 	}
-	/deep/.close-icon{
+
+	/deep/.close-icon {
 		position: absolute;
 		left: 1%;
-		top:4%;
+		top: 4%;
 	}
-	.container{
+
+	.container {
 		width: 100%;
 		height: 100%;
 		margin: 0 auto;
 		overflow: hidden;
 	}
+
 	.step-item-box {
 		width: 94%;
 		margin: 0 auto;
@@ -177,17 +202,17 @@ export default {
 		display: -webkit-flex;
 		margin-bottom: 1vmin;
 	}
-	
+
 	.step-item-box .step-item-left {
 		width: 10%;
 		position: relative;
 		min-height: 18vmin;
 	}
-	
+
 	.step-item-box .step-circle-undo.step-item-left:after {
 		border: none;
 	}
-	
+
 	.step-item-box .step-item-left:after {
 		content: ' ';
 		border-left: 1px solid #ddd;
@@ -198,11 +223,11 @@ export default {
 		top: 5vmin;
 		bottom: 0;
 	}
-	
+
 	.step-item-box .step-item-right {
 		width: 90%;
 	}
-	
+
 	.step-circle {
 		width: 3vmin;
 		height: 3vmin;
@@ -213,26 +238,26 @@ export default {
 		border-radius: 50%;
 		margin-top: 1.5vmin;
 	}
-	
+
 	.step-circle-undo .step-circle {
 		width: 2vmin;
 		height: 2vmin;
 		margin-left: 2.8vmin;
 		background: #ddd;
 	}
-	
+
 	.step-circle-doing .step-circle {
 		background: #00aca0;
 	}
-	
+
 	.step-circle-done .step-circle {
 		background: #ddd;
 	}
-	
+
 	.step-circle-done .step-circle .step-circle-inner {
 		background: #00ACA0;
 	}
-	
+
 	.step-circle .step-circle-inner {
 		width: 1vmin;
 		height: 1vmin;
@@ -243,23 +268,23 @@ export default {
 		transform: translate(-50%, -50%);
 		border-radius: 50%;
 	}
-	
+
 	.step-right-title span:first-child {
 		font-size: 14px;
 		color: #333;
 	}
-	
+
 	.step-right-title span:nth-child(2) {
 		font-size: 12px;
 		color: #999;
 		margin-left: 1vmin;
 	}
-	
+
 	.step-right-cont p {
 		font-size: 12px;
 		color: #999;
 	}
-	
+
 	.step-right-cont p span.cont-circle {
 		display: inline-block;
 		width: 1vmin;
@@ -269,8 +294,8 @@ export default {
 		margin-right: 1vmin;
 		vertical-align: middle;
 	}
-	
-	
+
+
 	.bot-alert-box {
 		position: fixed;
 		bottom: 0;
@@ -280,90 +305,105 @@ export default {
 		display: none;
 		z-index: 999
 	}
-	.pop-mask{
+
+	.pop-mask {
 		position: absolute;
 		width: 100%;
 		height: 100%;
 		background: black;
 		opacity: .5;
 	}
-	.pop-body{
+
+	.pop-body {
 		height: 40vh;
 		background: white;
 		position: absolute;
 		bottom: 0;
 		left: 0;
 		width: 100%;
-		
+
 		display: none;
 	}
-	.close-icon{
+
+	.close-icon {
 		width: 5vmin;
-	    height: 5vmin;
-	    position:absolute;
-	    left: 6vmin;
+		height: 5vmin;
+		position: absolute;
+		left: 6vmin;
 	}
-	.alert-title{
+
+	.alert-title {
 		text-align: center;
 		line-height: 15vmin;
 		font-size: 23px;
 	}
-	.alert-body{
+
+	.alert-body {
 		text-align: center;
 		position: absolute;
 		bottom: 6vh;
 		width: 100%
 	}
-	.alert-body-inp{
-	    width: 80%;
+
+	.alert-body-inp {
+		width: 80%;
 		height: 100%;
 		margin-top: 5%;
 	}
-	.alert-foot-del{
+
+	.alert-foot-del {
 		width: 50%;
 		background: #666;
 		height: 5vh;
 		display: inline-block;
 		position: absolute;
-	    bottom: 0;
+		bottom: 0;
 	}
-	.alert-foot-save{
+
+	.alert-foot-save {
 		width: 50%;
 		background: #999;
 		height: 5vh;
 		display: inline-block;
 		position: absolute;
-	    bottom: 0;
-	    right: 0;
+		bottom: 0;
+		right: 0;
 	}
-	.alert-foot-del,.alert-foot-save{
+
+	.alert-foot-del,
+	.alert-foot-save {
 		text-align: center;
 		vertical-align: middle;
 		line-height: 5vh;
 	}
-	.alert-foot{
+
+	.alert-foot {
 		display: flex;
 		display: -webkit-flex;
 	}
-	#btn{
-		background: rgba(0,0,0,0) !important;
+
+	#btn {
+		background: rgba(0, 0, 0, 0) !important;
 		position: relative;
 		left: 54vw;
 		height: 25px;
 		top: 3vh;
 	}
-	.body_step{
+
+	.body_step {
 		position: absolute;
 		height: 100vh;
 		width: 100vw;
 		overflow: scroll;
 	}
-	.block{
+
+	.block {
 		margin-left: 9vw;
 		margin-top: 7vh;
 	}
-	.head_card_icon{
-		background-image:url(../../assets/image/ManageA_details_cardicon.png) ;
+
+	.head_card_icon {
+		background-image: url(../../assets/image/ManageA_details_cardicon.png);
 		background-size: 100% 100%;
 		height: 10vh;
 		width: 10vh;
@@ -371,24 +411,27 @@ export default {
 		left: 5vw;
 		top: 5vh;
 		display: inline-block;
-		
+
 	}
-	.head_card{
+
+	.head_card {
 		background-image: url(../../assets/image/ManageA_details_cardbg.png);
 		background-size: 100% 100%;
 		width: 98vw;
 		margin: 0 auto;
 		height: 30vh;
 	}
-	.head_card_title{
+
+	.head_card_title {
 		position: relative;
 		left: 5vw;
 		top: 3vh;
 		font-size: 16px;
 		color: #fff;
 	}
-	.step_cicle1{
-		width:2vh;
+
+	.step_cicle1 {
+		width: 2vh;
 		height: 2vh;
 		background-color: pink;
 		position: relative;
@@ -396,14 +439,15 @@ export default {
 		text-align: center;
 		border-radius: 50%;
 	}
-	.step_cicle2{
-		width:0.6vh;
+
+	.step_cicle2 {
+		width: 0.6vh;
 		height: 0.6vh;
 		background-color: black;
 		position: absolute;
-		top:50%;
+		top: 50%;
 		left: 50%;
-		transform: translate(-50%,-50%);
+		transform: translate(-50%, -50%);
 		border-radius: 50%;
 	}
 </style>
--- src/components/financing/ManageO_add.vue
+++ src/components/financing/ManageO_add.vue
@@ -1,68 +1,280 @@
 <template>
 	<div>
-		<nut-navbar 
-			@on-click-back="back" 
-			@on-click-title="title" 
-			@on-click-more="more" 
-			:leftShow="true" 
-			:rightShow="false"
-		>添加机构</nut-navbar>
-		<div class="body">
-			<dvi class="tabel-title">
-				
-				<div class="text_style"></div>
-			</dvi>
+		<nut-navbar @on-click-back="back" @on-click-title="title" @on-click-more="more" :leftShow="true" :rightShow="false">添加机构</nut-navbar>
+		<div class="list-body">
+			<div class="list1">
+				<div class="title-box">
+					<img src="../../assets/image/line1.png" alt="" class="line">
+					<span class="title-text">机构名称与LOGO</span>
+				</div>
+				<div class="up-image">
+					<span style="" class="list-left">上传图片</span>
+					<el-upload class="avatar-uploader" action="https://jsonplaceholder.typicode.com/posts/" :show-file-list="false"
+					 :on-success="handleAvatarSuccess" :before-upload="beforeAvatarUpload" style="text-align: right;">
+						<img v-if="imageUrl" :src="imageUrl" class="avatar">
+						<!-- <img v-else src="../../assets/image/up-image.jpg" alt="" class="up-img-icon"> -->
+						<div class="up-img-icon" style="height: 5vh;"></div>
+					</el-upload>
+				</div>
+				<div class="mechanism">
+					<nut-textinput placeholder="请输入内容" label="机构名称" suffix="aaa" v-model="val" />
+				</div>
+				<div class="mechanism">
+					<nut-textinput placeholder="请输入内容" label="机构所在地" v-model="val" />
+				</div>
+			</div>
+
+			<div class="list2">
+				<div class="title2-box">
+					<img src="../../assets/image/line1.png" alt="" class="line">
+					<span class="title-text">机构简介</span>
+				</div>
+			</div>
+			<div @click="gopersonage()">
+				<nut-cell title="项目简介" desc="请输入项目简介" :showIcon="true" class="projectJS"></nut-cell>
+			</div>
+			<div class="title3-box">
+
+				<img src="../../assets/image/line1.png" alt="" class="line">
+				<span class="title-text">投资行业</span>
+
+				<span class="add-icon" @click="add_tag(1)">&nbsp;+&nbsp;</span>
+			</div>
+			<div class="title4-box">
+				<img src="../../assets/image/line1.png" alt="" class="line">
+				<span class="title-text">投资行业</span>
+				<span class="add-icon" @click="add_tag(2)">&nbsp;+&nbsp;</span>
+			</div>
 		</div>
-		
+		<div class="btn">
+			<nut-button block shape="circle">
+				登录
+			</nut-button>
+		</div>
+		<nut-picker :is-visible="is_show" :default-value-data="defaultValueData1" :list-data="listData0" @close="switchPicker('isVisible1')"
+		 @confirm="setYearValue">
+		</nut-picker>
+
 	</div>
 </template>
 <script>
 	export default {
+		data() {
+			return {
+				is_show: false,
+				val: '',
+				imageUrl: '',
+				listData0: [
+					['2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021', '2022', '2023'],
+					['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+				],
+				defaultValueData1: ['2012', '2'],
+			};
+		},
 		methods: {
-			back(){
+			back() {
 				alert('header头部， 点击返回')
 			},
-			title(){
+			title() {
 				alert('header头部， 点击title')
 			},
-			more(){
+			more() {
 				alert('header头部， 点击更多')
-			}
+			},
+			handleAvatarSuccess(res, file) {
+				this.imageUrl = URL.createObjectURL(file.raw);
+			},
+			beforeAvatarUpload(file) {
+				const isJPG = file.type === 'image/jpeg';
+				const isLt2M = file.size / 1024 / 1024 < 2;
+
+				if (!isJPG) {
+					this.$message.error('上传头像图片只能是 JPG 格式!');
+				}
+				if (!isLt2M) {
+					this.$message.error('上传头像图片大小不能超过 2MB!');
+				}
+				return isJPG && isLt2M;
+			},
+			gopersonage() {
+				console.log(11111);
+			},
+			add_tag(val) {
+				if (val == 1) {
+					console.log(111);
+					this.is_show = true;
+				} else if (val == 2) {
+					this.is_show = true;
+				} else {
+					console.log('fail');
+				}
+			},
+			switchPicker(param) {
+				this.is_show = false;
+			},
+			setYearValue(chooseData) {
+				this.is_show = false;
+			},
+
+
 		}
 	};
 </script>
 
 <style lang="scss" scoped>
-	/deep/.nut-navbar{
+	/deep/.nut-button.block {
+		width: 80%;
+	}
+
+	/deep/.nut-button {
+		background: pink;
+	}
+
+	/deep/.nut-navbar {
 		height: 7vh;
 	}
-	/deep/.nav-title{
-		font-size: 18px;
+
+	/deep/.nav-title {
+		font-size: 16px;
 	}
-	/deep/.nut-cell-title{
+
+	/deep/.nut-cell-title {
 		margin-left: 3vmin;
 	}
-	.body{
-		height: 97vh;
+
+	/deep/.nut-cell-box {
+		margin-left: -2vh;
 	}
-	.tabel-title{
-		height: 2vmin;
-		width: 100%;
-		background:#fff;
+
+	/deep/.nut-uploader {
+		font-size: 16px;
+		margin-left: 3vw;
 	}
-	.text_style{
-		height: 80%;
-		background-image: url("../../assets/image/line1.png");
-		background-size: 1%;
-		background-repeat: no-repeat;
-		display: inline-block;
-		vertical-align: middle;
-		width: 100%;
-		height: 15vmin;
+
+	/deep/.nut-textinput input {
+		border: 0px;
+		font-size: 13px;
+	}
+
+	/deep/.nut-textinput span {
+		line-height: 8vh;
+		font-size: 16px;
+		margin-left: 2vw;
+	}
+
+	/deep/.nut-textinput>input {
+		text-align: right;
+		font-size: 16px;
+	}
+
+	/deep/.nut-cell-left>span {
+		font-size: 16px !important;
+	}
+
+	/deep/.nut-cell-right>span {
+		font-size: 16px;
+	}
+
+	.btn {
+		text-align: center;
+	}
+
+	.add-icon {
+		border-radius: 50%;
+		border: 1px solid blue;
+		height: 2vh;
+		line-height: 8vh;
+		margin-left: 65vw;
+	}
+
+	.projectJS {
+		height: 8vh;
+	}
+
+	.list-left {
+		line-height: 8vh;
+		font-size: 16px;
+		margin-left: 2vw;
+	}
+
+	.mechanism {
+		height: 8vh;
+		border-bottom: 1px solid #666;
+	}
+
+	.up-img-icon {
 		position: absolute;
-		left: 0;
-		right: 0;
-		margin-left: auto;
-		margin-right: auto;
+		top: 18vh;
+		width: 5vh;
+		right: 5vh;
+		background-image: url("../../assets/image/up-image.jpg");
+		height: 5vh;
+		background-size: 100% 100%;
+	}
+
+	.title-text,
+	.line {
+		vertical-align: middle;
+		line-height: 8vh;
+	}
+
+	.title-text {
+		margin-left: 2%;
+		font-size: 16px
+	}
+
+	.title-box,
+	.title2-box,
+	.title3-box,
+	.title4-box {
+		margin-top: 3%;
+		padding-left: 3%;
+		border-bottom: 1px solid #efefef;
+		height: 8vh;
+		background: #fff;
+	}
+
+	.list1 {
+		background: #fff;
+		width: 100%;
+		height: 45%;
+	}
+
+	.up-image {
+		width: 100%;
+		height: 8vh;
+		border-bottom: 1px solid;
+	}
+
+	.list-body {
+		width: 100%;
+		height: 80vh;
+	}
+
+	.avatar-uploader .el-upload {
+		border: 1px dashed #d9d9d9;
+		border-radius: 6px;
+		cursor: pointer;
+		position: relative;
+		overflow: hidden;
+	}
+
+	.avatar-uploader .el-upload:hover {
+		border-color: #409EFF;
+	}
+
+	.avatar-uploader-icon {
+		font-size: 28px;
+		color: #8c939d;
+		width: 178px;
+		height: 178px;
+		line-height: 178px;
+		text-align: center;
+	}
+
+	.avatar {
+		width: 178px;
+		height: 178px;
+		display: block;
 	}
 </style>
